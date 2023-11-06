98abe12a30d2ec68d90478f823b5e70bd5d2f472
Yuri Caetano <yuuri.caetano@gmail.com>
2023-11-06 18:17:33 -0300

    feat: finish all 4 lp

---
+++ landing_7.1l_frango/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          default: "#981F1F",
+        },
+        black: {
+          default: "#1D1D1B",
+          black: "#000000",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        "material-icons": ["Material Symbols Outlined"],
+      },
+      maxWidth: {
+        vtex: "74rem",
+      },
+      backgroundImage: {
+        leftSign: "url('./images/icons/left_sign.png')",
+        rightSign: "url('./images/icons/right_sign.png')",
+      },
+    },
+  },
+  plugins: [],
+};

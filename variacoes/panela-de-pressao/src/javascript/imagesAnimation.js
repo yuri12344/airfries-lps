a26d3bc79f0afedd65dec585eb54ba20801f6e49
Yuri Caetano <yuuri.caetano@gmail.com>
2023-11-14 09:09:08 -0300

    feat: update panela de pressao

--- variacoes/panela-de-pressao/src/javascript/imagesAnimation.js
+++ variacoes/panela-de-pressao/src/javascript/imagesAnimation.js
@@ -6,12 +6,15 @@ window.addEventListener("message", function (event) {
   animatedElements.forEach((element, index) => {
     let newTransform;
     const widerFactor = 10; // Adjust this value to control the range of movement
+    const subtleFactor = 2; // Adjust this value to control the range of movement
 
     if (element.classList.contains("animate-float")) {
       newTransform = Math.sin(scrollY / 100 + index) * widerFactor * 10; // Multiplied by 10 for more noticeable movement
     } else if (element.classList.contains("animate-float-reverse")) {
       newTransform = Math.cos(scrollY / 100 + index) * widerFactor * 10; // Multiplied by 10 for more noticeable movement
-    }
+    } else if (element.classList.contains("animate-subtle")) {
+      newTransform = Math.cos(scrollY / 100 + index) * subtleFactor * 10;
+    } 
     element.style.transform = `translateY(${newTransform}px)`;
   });
 });
